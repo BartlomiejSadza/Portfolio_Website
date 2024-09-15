75f5ac1fa17592cf661b0556f4fb15284a740fab
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-09-15 20:41:32 +0200

    Navbar skeleton

---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <header className="App-header">
+      <div className="logo">
+        <img src="./img/devjane.png" className="App-logo" alt="logo" />
+      </div>
+      <button className="nav-toggle" aria-label="toggle navigation">
+        <span className="hamburger"></span>
+      </button>
+      <nav className="nav">
+        {/* Nav Tabs */}
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              My Projects
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              About Me
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              My Work
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
