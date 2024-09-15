887bdc9ad17afe207683dad09877a076a9f5a8ed
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-09-16 00:33:31 +0200

    Basic logic added

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -2,25 +2,58 @@ import React from "react";
 
 export default function MyWork() {
 
-  function images() {
-    const imageElements = [];
-    for (let i = 0; i < 9; i++) {
-      imageElements.push(
-        <div className="portfolio" key={i}>
-          <a href="#" className="portfolio__item">
-            <img src={`../img/portfolio-0${i + 1}.jpg`} alt="" className="portfolio__img" />
-          </a>
-        </div>
-      );
-    }
-    return imageElements;
-  }
-
   return (
     <section className="my-work" id="work">
       <h2 className="section__title section__title--work">My Work</h2>
       <p className="section__subtitle section__subtitle--work">A selection of my range of work</p>
-      {images()}
+      <div className="portfolio">
+        {/* Portfolio item 01 */}
+        <></>
+        {/* Portfolio item 02 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-02.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 03 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-03.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 04 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-04.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 05 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-05.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 06 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-06.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 07 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-07.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 08 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-08.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 09 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-09.jpg" alt="" className="portfolio__img" />
+        </a>
+        
+        {/* Portfolio item 10 */}
+        <a href="#" className="portfolio__item">
+          <img src="img/portfolio-10.jpg" alt="" className="portfolio__img" />
+        </a>
+      </div>
     </section>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,48 @@
 import React from "react";
 
 export default function Nav() {
+ 
+  const navToggle = document.querySelector('.nav-toggle');
+  const toggleNav = () => {
+    document.body.classList.toggle("nav-open");
+  };
+
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+  })
+
   return (
     <header className="App-header">
       <div className="logo">
         <img src="./img/_.svg" className="App-logo" alt="logo" />
       </div>
-      <button className="nav-toggle" aria-label="toggle navigation">
+      <button onClick={() => toggleNav()} className="nav-toggle" aria-label="toggle navigation">
         <span className="hamburger"></span>
       </button>
       <nav className="nav">
         {/* Nav Tabs */}
         <ul className="nav__list">
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="#home" className="nav__link">
               Home
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="#services" className="nav__link">
               My Services
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="#about" className="nav__link">
               About Me
             </a>
           </li>
           <li className="nav__item">
-            <a href="#" className="nav__link">
+            <a href="#work" className="nav__link">
               My Work
             </a>
           </li>
