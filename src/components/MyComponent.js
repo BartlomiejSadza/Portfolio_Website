d17a92c24015f786a28f032d00205e46c56d6908
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-09-16 13:37:33 +0200

    popup styling done, time to fulfill it

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -25,32 +25,41 @@ function MyComponent(props) {
           <div
             style={{
               maxHeight: "80vh",
+              width: "80vw",
+              maxWidth: "1000px",
               position: "absolute",
               top: "50%",
               left: "50%",
               transform: "translate(-50%, -50%)",
               backgroundColor: "white",
-              padding: "2em",
+              padding: "0.5em",
               overflowY: "auto",
             }}
           >
             {/* Here some thing bout projects */}
+            <button
+              onClick={closePopup}
+              className="nav-toggle nav-open dot"
+              aria-label="toggle navigation"
+            >
+              <span className="hamburger"></span>
+            </button>
             <section class="intro">
-            <h1 class="section__title section__title--intro">
+              <h1 class="section__title section__title--intro">
                 The title <strong>of my project</strong>
-            </h1>
-            <p class="section__subtitle section__subtitle--intro">A short subtitle</p>
-            <img src="img/portfolio-01.jpg" alt="" class="intro__img" />
-        </section>
-        
-        <div class="portfolio-item-individual">
-            <p>opis</p>
-            <img src="img/portfolio-details.jpg" alt="" />
-            <p>opis2</p>
-            <p>bottom description</p>
-        </div>
-            <h2>Pop-up Content</h2>
-            <button onClick={closePopup}>Close Pop-up</button>
+              </h1>
+              <p class="section__subtitle section__subtitle--intro">
+                A short subtitle
+              </p>
+              <img src="img/portfolio-01.jpg" alt="" class="intro__img" />
+            </section>
+
+            <div class="portfolio-item-individual">
+              <p>opis</p>
+              <img src="img/portfolio-01.jpg" alt="" />
+              <p>opis2</p>
+              <p>bottom description</p>
+            </div>
           </div>
         </div>
       )}
