21088810cebe2dd0962b10b20a3342c146b777bc
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2024-11-28 17:45:45 +0100

    dodane Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import Intro from "./components/Intro";
@@ -6,8 +6,15 @@ import MyServices from "./components/MyServices";
 import AboutMe from "./components/AboutMe";
 import MyWork from "./components/MyWork";
 import Footer from "./components/Footer";
+import ReactGA from "react-ga4"
 
 function App() {
+
+    useEffect(() => {
+        ReactGA.initialize("G-MB11JLSWMZ");
+        ReactGA.send("pageview");
+    }, []);
+
   return (
     <div className="App">
       <Nav /> 
